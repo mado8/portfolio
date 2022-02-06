47eb4ef8ea6df399e511febd1b4c6f119ba6abc2
mado <madelineadonley@gmail.com>
2022-02-05 17:07:07 -0700

    added portfolio and va items to main container

--- portfolio/src/components/MainContainter.jsx
+++ portfolio/src/components/MainContainter.jsx
@@ -2,18 +2,153 @@ import React, { useState } from 'react'
 import Navbar from './Navbar/Nav'
 import Intro from './Introduction/Intro'
 import About from './About/About'
+import Skills from './Skills/Skills'
+import Projects from './Portfolio/Projects'
 
-function MainContainer() {
+const navItems = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#intro',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about-me',
+  },
+  {
+    id: 3,
+    name: 'Skills',
+    href: '#skills',
+  },
+  {
+    id: 5,
+    name: 'Portfolio',
+    href: '#portfolio',
+  },
+  {
+    id: 6,
+    name: 'Contact Me',
+    href: '#contact-me',
+  },
+]
+
+const projectList = [
+  {
+    id: 1,
+    name: 'Employee Tracker',
+    description:
+      'This employee tracker allows the user to imput information and generate an HTML page with easy access.',
+    size: 'small-project',
+    buttons: [
+      {
+        name: 'GitHub',
+        href: 'https://github.com/mado8/employee-tracker',
+      },
+      {
+        name: 'Live Url',
+        href: 'https://mado8.github.io/employee-tracker/',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Dirt!',
+    description: '',
+    size: 'large-project',
+    buttons: [
+      {
+        name: 'GitHub',
+        href: 'https://github.com/Josey9087/Dirt',
+      },
+      {
+        name: 'Live Url',
+        href: 'https://dirt93873.herokuapp.com/',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Management System',
+    description: '',
+    size: 'large-project',
+    buttons: [
+      {
+        name: 'GitHub',
+        href: 'https://github.com/mado8/employee-management-system',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'E-Commerce Backend',
+    description: '',
+    size: 'small-project',
+    buttons: [
+      {
+        name: 'GitHub',
+        href: 'https://github.com/mado8/e-commerce-back-end',
+      },
+      {
+        name: 'Live Url',
+        href: '#',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Game Go',
+    description: '',
+    size: 'small-project',
+    buttons: [
+      {
+        name: 'GitHub',
+        href: 'https://github.com/mthurber82/videogameapp',
+      },
+      {
+        name: 'Live Url',
+        href: 'https://mthurber82.github.io/videogameapp/',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Work Day Scheduler',
+    description: '',
+    size: 'large-project',
+    buttons: [
+      {
+        name: 'GitHub',
+        href: 'https://github.com/mado8/work-calendar',
+      },
+      {
+        name: 'Live Url',
+        href: 'https://mado8.github.io/work-calendar/',
+      },
+    ],
+  },
+]
+
+const MainContainer = () => {
+  const [page, setPage] = useState(false)
+  const handlePage = (page) => {
+    setPage(page)
+  }
+
   return (
     <div id='main-container'>
       <div className='row sub-containers g-0'>
-          <Navbar />
+        <div>
+          <Navbar navItems={navItems} handlePage={handlePage} />
+        </div>
+          <div>
             <Intro />
             <About />
+            <Skills />
+            <Projects projectList={projectList} />
           </div>
-        <div>
-        </div>
       </div>
+    </div>
   )
 }
 
